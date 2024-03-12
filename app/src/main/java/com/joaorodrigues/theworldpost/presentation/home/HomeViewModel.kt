0e887dcd5b4fc3eb0cd24ca3d2bff595cdd340ab
João Paulo da Silva Rodrigues <joaopaulo.rodrigues@a4pm.com.br>
2024-03-11 21:19:07 -0300

    Finishing app

--- app/src/main/java/com/joaorodrigues/theworldpost/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/joaorodrigues/theworldpost/presentation/home/HomeViewModel.kt
@@ -1,6 +1,5 @@
 package com.joaorodrigues.theworldpost.presentation.home
 
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
